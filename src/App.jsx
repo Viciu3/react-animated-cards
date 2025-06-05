df59766a47af2287654bd6f820037ec74aebb7ca
Т̷е̷н̷ьSAQM <kurichich5@gmail.com>
2025-06-05 19:51:39 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardList from './components/CardList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Анимированные карточки</h1>
+      <CardList />
+    </div>
+  );
+}
+
+export default App;
